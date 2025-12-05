b583d79dfc11046772cdf6fae63ea1996d034539
dfcut8 <dfcut8@proton.me>
2025-12-05 11:56:27 -0500

    movement works, but too slow

--- Scenes/Player/Player.cs
+++ Scenes/Player/Player.cs
@@ -12,8 +12,46 @@ public partial class Player : CharacterBody2D
 
     private Vector2 direction = Vector2.Zero;
 
+    public override void _Process(double delta)
+    {
+        PlayerMovement((float)delta);
+    }
+
+    private Vector2 IsometricMovement(Vector2 direction)
+    {
+        var adjustedDirection = Vector2.Zero;
+        adjustedDirection.X = direction.X - direction.Y;
+        adjustedDirection.Y = (direction.X + direction.Y) / 2;
+        return adjustedDirection;
+    }
+
     private void PlayerMovement(float delta)
     {
-        direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+        direction = Input.GetVector(
+            "player_move_left",
+            "player_move_right",
+            "player_move_up",
+            "player_move_down"
+        );
+
+        if (direction != Vector2.Zero)
+        {
+            Velocity = Velocity.LimitLength(Speed);
+        }
+
+        if (direction == Vector2.Zero)
+        {
+            if (Velocity.Length() > (friction * delta))
+            {
+                Velocity = Velocity.Normalized() * (friction * delta);
+            }
+            else
+            {
+                Velocity = Vector2.Zero;
+            }
+        }
+
+        Velocity = IsometricMovement(direction * acceleration * delta);
+        MoveAndSlide();
     }
 }
